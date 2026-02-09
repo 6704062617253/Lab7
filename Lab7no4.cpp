8efff8dbbe8c58ac3ae2dc1853977f79abf36a36
6704062617253-Itsarapong <s6704062617253@email.kmutnb.ac.th>
2026-02-09 23:04:06 +0700

    Lab7no4
    Square of stars

---
+++ Lab7no4.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void printHead(int w);
+void printBody(int w, int h);
+
+int main() {
+    int width, height;
+    
+    printf("Enter width and height: ");
+    scanf("%d %d", &width, &height);
+    
+    printHead(width);
+    printBody(width, height);
+    printHead(width);
+    
+    return 0;
+}
+
+void printHead(int w) {
+    for(int i = 0; i < w; i++) {
+        printf("* ");
+    }
+    printf("\n");
+}
+
+void printBody(int w, int h) {
+    for(int i = 0; i < h; i++) {
+        printf("*");
+        for(int j = 0; j < w - 2; j++) {
+            printf("  ");
+        }
+        printf(" *\n");
+    }
+}
